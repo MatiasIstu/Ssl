9976f19a84e5b1fbf3bf3fa29ac366ded29e20b2
MatiasIstu <32072030+MatiasIstu@users.noreply.github.com>
2018-04-24 17:33:36 -0300

    Update atr.c

--- atr.c
+++ atr.c
@@ -3,24 +3,29 @@
 
 int main(int argC, char* argV[])
 {
-    if(!(argC%2)) 
-    {
-            printf("ERROR: Se ingreso un numero incorrecto de archivos")
-    }
-    else
-    {
-        printf("%s",argV[1]);
-        for(int i=0;i<(argC-1)/2,i++){
-            FILE* f;
-            f = fopen(argV[i],"r");
-            //Leer archivo
-            fclose(f);
-            i++;
-            f = fopen(argV[i],"w");
-            //Escribir archivo
-            fclose(f);
+    if(!(argC%2)){
+        printf("ERROR: Se ingreso un numero incorrecto de archivos");
+    }else{
+        for(int i=1;i<=(argC-1);i+=2){
+            FILE* archivoTexto;
+            FILE* archivoBinario;
+            int caracterLeido;
+            if(!(archivoTexto = fopen(argV[i],"r"))){
+                printf("No se pudo abrir el archivo %s \n",argV[i]);
+                break;
+            }
+            if(!(archivoBinario = fopen(argV[i+1],"wb"))){
+                printf("No se pudo abrir el archivo %s \n",argV[i+1]);
+                break;
+            }
+            while(caracterLeido != EOF){
+                caracterLeido = fgetc(archivoTexto);
+                fwrite(&caracterLeido,sizeof(int),1,archivoBinario);
+            }
+            fclose(archivoTexto);
+            fclose(archivoBinario);
+            caracterLeido = 0;
         }
     }
     return 0;
 }
-
